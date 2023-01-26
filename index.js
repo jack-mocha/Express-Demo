6914e2c2b6c8c25a20ece5f91064807d40fa2859
Jack Hsu <jackhsu0225@gmail.com>
2023-01-26 11:44:51 -0500

    Demonstrate setting status code on the response

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
@@ -10,8 +16,12 @@ app.get('/api/courses', (req, res) => {
 });
 
 //single parameter
-app.get('/api/course/:id', (req, res) => {
-    res.send(req.params.id);
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course)
+        res.status(404).send('Not Found');
+
+    res.send(course);
 });
 
 //multiple parameters
