4286742375e987ada6fe1a7b9bf071201ebe4189
Jack Hsu <jackhsu0225@gmail.com>
2023-01-26 11:37:04 -0500

    Add end points with parameters. Access query string

--- index.js
+++ index.js
@@ -9,5 +9,17 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
+//single parameter
+app.get('/api/course/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+//multiple parameters
+//access query
+app.get('/api/posts/:year/:month', (req, res) => {
+    // res.send(req.params);
+    res.send(req.query);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
