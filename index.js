892341b8f7fc4b1237b16f11a2ec039750c931fa
Jack Hsu <jackhsu0225@gmail.com>
2023-01-29 15:21:47 -0500

    Add an endpoint to delete a course

--- index.js
+++ index.js
@@ -66,6 +66,17 @@ app.put('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course)
+        res.status(404).send('Not Found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);    
+});
+
 function validateCourse(course){
     const schema = Joi.object({
         name: Joi.string().min(3).required()
